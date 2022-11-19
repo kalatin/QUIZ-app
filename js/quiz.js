3c5308b1384d7939f9cbb59654fe25ad800f45f0
kalatin <pitsik228@mail.ru>
2022-11-19 17:47:04 +0300

    what if this final question

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,7 @@ class Quiz {
 	constructor (survey) {
 		this.survey = survey;
 		this.score = 0;
-		this.currentQuestion = 0;
+		this.currentQuestion = 3;
 
 		this.indicator = document.querySelector('.quiz__indicator');
 		this.info = document.querySelector('.quiz__question');
@@ -11,30 +11,44 @@ class Quiz {
 	};
 
 	render () {
-		//Индикатор
-		this.indicator.textContent = `${this.currentQuestion + 1}/${this.survey.length}`;
-		//Вопрос
-		this.info.textContent = this.survey[this.currentQuestion].question;
-		//Ответы
-		for (let i = 0; i < this.survey[this.currentQuestion].answers.length; i++) {
-			let option = document.createElement('div');
-			option.classList.add('quiz__option');
-			option.innerHTML = `<label><input type="radio" name="ex1" value="${i + 1}"><p>${this.survey[this.currentQuestion].answers[i]}</p></label>`;
-			this.list.insertAdjacentElement(
-				'beforeend',
-				option
-			)
-		};
-		this.list.querySelectorAll('label').forEach(item =>{ 
-			item.addEventListener('click', () =>{
-				if (item.querySelector('input').checked) {
-					this.list.querySelectorAll('label').forEach(el=>{ el.classList.remove('active'); });
-					item.classList.add('active')
-				}
+		//Если это последний вопрос
+		if(this.currentQuestion == this.survey.length){
+			//Индикатор
+			this.indicator.textContent = `Тест завершен`;
+			//Сколько ответов
+			this.info.textContent = `Вы ответили правильно на ${this.score} из ${this.survey.length} вопросов`;
+			//Очистка листа
+			this.list.innerHTML = '';
+			//Кнопка ещё раз
+			this.button.textContent = `Ещё раз`;
+		} 
+		//Если нет
+		else {
+			//Индикатор
+			this.indicator.textContent = `${this.currentQuestion + 1}/${this.survey.length}`;
+			//Вопрос
+			this.info.textContent = this.survey[this.currentQuestion].question;
+			//Ответы
+			for (let i = 0; i < this.survey[this.currentQuestion].answers.length; i++) {
+				let option = document.createElement('div');
+				option.classList.add('quiz__option');
+				option.innerHTML = `<label><input type="radio" name="ex1" value="${i + 1}"><p>${this.survey[this.currentQuestion].answers[i]}</p></label>`;
+				this.list.insertAdjacentElement(
+					'beforeend',
+					option
+				)
+			};
+			this.list.querySelectorAll('label').forEach(item =>{ 
+				item.addEventListener('click', () =>{
+					if (item.querySelector('input').checked) {
+						this.list.querySelectorAll('label').forEach(el=>{ el.classList.remove('active'); });
+						item.classList.add('active')
+					}
+				})
 			})
-		})
-		//Кнопка
-		this.button.textContent = 'Ответить'
+			//Кнопка
+			this.button.textContent = 'Ответить';
+		}
 	}
 }
 
