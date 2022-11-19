4f11c94ed258eb84be9abd014c3651bfe4f957e5
kalatin <pitsik228@mail.ru>
2022-11-19 16:54:05 +0300

    add dom structure to class

--- js/quiz.js
+++ js/quiz.js
@@ -3,5 +3,10 @@ class Quiz {
 		this.survey = survey;
 		this.score = 0;
 		this.currentQuestion = 0;
+
+		this.indicator = document.querySelector('.quiz__indicator');
+		this.info = document.querySelector('.quiz__question');
+		this.list = document.querySelector('.quiz__list');
+		this.button = document.querySelector('.quiz__button > button');
 	}
 }
